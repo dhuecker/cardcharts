9b660efa6de993768f27cbfb99daeca34a6ffef1
David Huecker <dh001@ucr.edu>
2021-03-01 20:19:58 -0800

    updated

--- scraptodbNFL.py
+++ scraptodbNFL.py
@@ -1,23 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import sqlite3
-conn = sqlite3.connect('cardcharts')
-c = conn.cursor()
-#c.execute('''CREATE TABLE nfl(title TEXT, soldprice REAL, solddate TEXT)''')
+import pyrebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import datetime 
+from datetime import datetime
+import pytz
+
+#tz = pytz.timezone*'United'
+
+cred = credentials.Certificate("cardchar-edd6a-firebase-adminsdk-pld8f-edafff092b.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+# Temporarily replace quote function
+def noquote(s):
+    return s
+pyrebase.pyrebase.quote = noquote
+
+card = db.collection(u'cards').document(u'tom brady')
+
+card.set({
+    u'soldprice': True
+}, merge=True)
+
+
 def main():
-    searchterm = 'aaron+rodgers+rookie+card'
+
+    
+    
+
+    searchterm = '+brees+rookie+card'
     url = f"https://www.ebay.com/sch/i.html?_from=R40&_nkw={searchterm}&_sacat=0&rt=nc&LH_Sold=1&LH_Complete=1"
     soup = get_data(url)
-    products = parse(soup)
-    conn.commit()
-    print('complete.')
-    c.execute('''SELECT * FROM nfl''')
-    results = c.fetchall()
-    print(results)
-   # export(products, searchterm)
+    products = parse(soup, searchterm)
+    #temp = db.child("cards").order_by_child("title").start_at("tom brady").order_by_child("soldprice").start_at(100).limit_to_first(2).get()
+    #print(temp.val())
+
+    #export(products, searchterm)
 
 def get_data(url):
+
     r = requests.get(url)
     if r.status_code != 200:
         print('Failed to get data: ', r.status_code)
@@ -26,7 +51,10 @@ def get_data(url):
         print(soup.title.text)
     return soup
 
-def parse(soup):
+def parse(soup, searchterm):
+    search = "tom brady"
+    collection_id = "cards"
+    i = 0
     productlist = []
     results = soup.find_all('div', {'class': 's-item__info clearfix'})
     for item in results:
@@ -37,12 +65,16 @@ def parse(soup):
         
         'link': item.find('a', {'class': 's-item__link'})['href']        
         }
-        temp = item.find('h3', {'class':'s-item__title s-item__title--has-tags'}).text
+        temp1 = item.find('h3', {'class':'s-item__title s-item__title--has-tags'}).text
         temp2 =  float(item.find('span', {'class':'s-item__price'}).text.replace('$','').replace(',','').strip())
         temp3 = item.find('span', {'class': 's-item__title--tagblock__COMPLETED'}).find('span', {'class':'POSITIVE'}).text
-        c.execute('''INSERT INTO nfl VALUES(?,?,?)''',(temp, temp2, temp3))
+     
+        
         productlist.append(products)
-        #print(products)
+    
+        data = {"title": temp1, "soldprice":temp2, "solddate": temp3, "sitesold": "ebay"}
+        db.collection(collection_id).add(data)
+        i = i +1
     return productlist
 
 def loadup(productlist):
